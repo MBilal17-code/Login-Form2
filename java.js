2b8e5ad1a6e9a6a02fd4a8473656c8ff0cc57ffc
MBilal17-code <malikbilalbf7@gmail.com>
2023-08-25 17:15:23 +0500

    Again

---
+++ java.js
@@ -0,0 +1,11 @@
+function show()
+{
+  let email = document.getElementById("email").value;
+  let password = document.getElementById("password").value;
+  let image = document.querySelector("img");
+  console.log(email);
+  console.log(password);
+  console.log(image.src);
+}
+
+
